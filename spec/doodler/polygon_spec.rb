f735076c286b18e0f0357fa85577d345162de940
Marshall Shen <fengyuan-shen@uiowa.edu>
2013-02-17 11:37:16 -0600

    WIP Polygon

---
+++ spec/doodler/polygon_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Doodler::Polygon do
+  describe "#add_point" do
+    context "given 1 exisiting point" do
+      it "should draw another point that is within a certain radius of the first point"
+    end
+  end
+end
